421c5547312c94c7e2a2c3c70af4e5a4334b52b0
Tony Morris <tmorris@tmorris.net>
2010-09-20 15:57:53 +1000

    More parsing libraries

---
+++ L06/MoreParser.hs
@@ -0,0 +1,26 @@
+module L06.MoreParser where
+
+import L03.Parser
+import Control.Applicative
+
+instance Functor Parser where
+  fmap f p = bindParser p (valueParser . f)
+
+instance Applicative Parser where
+  pure = valueParser
+  f <*> a = bindParser f (\f' ->
+            bindParser a (\a' ->
+            valueParser (f' a')))
+
+instance Monad Parser where
+  return = pure
+  (>>=) = bindParser
+
+thenSpaces ::
+  Parser a
+  -> Parser a
+thenSpaces p =
+  do v <- p
+     spaces
+     return v
+
