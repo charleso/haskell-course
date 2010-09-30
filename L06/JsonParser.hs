0e633e38ddde2f317cdfbc5e0d5894e168040321
Tony Morris <tmorris@tmorris.net>
2010-09-30 12:03:28 +1000

    Tidying up

--- L06/JsonParser.hs
+++ L06/JsonParser.hs
@@ -2,9 +2,7 @@ module L06.JsonParser where
 
 import Prelude hiding (exponent)
 import Numeric
-import Data.Char
 import Control.Applicative
-import Control.Monad
 import L01.Validation
 import L03.Parser
 import L06.JsonValue
@@ -13,22 +11,9 @@ import L06.MoreParser
 jsonString ::
   Parser String
 jsonString =
-  let c =     (is '\\' >> e)
+  let e = oneof "\"\\/bfnrt" ||| hex
+      c = (is '\\' >> e)
           ||| satisfyAll [(/= '"'), (/= '\\')]
-      e =     '"'  <$ is '"'
-          ||| '\\' <$ is '\\'
-          ||| '/'  <$ is '/'
-          ||| '\b' <$ is 'b'
-          ||| '\f' <$ is 'f'
-          ||| '\n' <$ is 'n'
-          ||| '\r' <$ is 'r'
-          ||| '\t' <$ is 't'
-          ||| is 'u' *> u
-      u = do d <- replicateM 4 (satisfy isHexDigit)
-             let z = fst . head . readHex $ d
-             if z <= (fromEnum (maxBound :: Char))
-                then pure (toEnum z)
-                else failed ("Expected valid character. Found: " ++ [chr z])
   in betweenCharTok '"' '"' (list c)
 
 jsonNumber ::
