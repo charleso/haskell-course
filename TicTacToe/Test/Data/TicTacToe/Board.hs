52199f065309360dc6231d3dc7b746383e11e360
Tony Morris <tmorris@tmorris.net>
2010-11-17 17:58:15 +1000

    Board tests

---
+++ TicTacToe/Test/Data/TicTacToe/Board.hs
@@ -0,0 +1,28 @@
+module Test.Data.TicTacToe.Board
+(
+  main
+, boardTests
+) where
+
+import Data.TicTacToe.Board
+import Test.QuickCheck
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+instance Arbitrary Board where
+  arbitrary = undefined
+
+main ::
+  IO ()
+main =
+  defaultMain boardTests
+
+boardTests ::
+  [Test]
+boardTests =
+  [
+    testGroup "Board"
+      [
+      -- todo
+      ]
+  ]
