4efeae6b6d3bda5f066dad4ce86eedc4461ad0a1
Tony Morris <tmorris@tmorris.net>
2010-11-18 18:08:22 +1000

    Haddock
    --HG--
    branch : craig-aspinall

--- TicTacToe/Data/TicTacToe/Player.hs
+++ TicTacToe/Data/TicTacToe/Player.hs
@@ -1,3 +1,4 @@
+-- | A player is either player 1 or player 2 /(isomorphic to Bool)/.
 module Data.TicTacToe.Player
 (
 Player
@@ -10,6 +11,7 @@ Player
 , toSymbol
 ) where
 
+-- | A player.
 data Player =
   Player1
   | Player2
@@ -19,6 +21,7 @@ instance Show Player where
   show Player1 = "Player 1"
   show Player2 = "Player 2"
 
+-- | Returns whether or not the player is player 1.
 isPlayer1 ::
   Player ->
   Bool
@@ -27,40 +30,46 @@ isPlayer1 Player1 =
 isPlayer1 Player2 =
   False
 
+-- | Returns whether or not the player is player 2.
 isPlayer2 ::
   Player
   -> Bool
 isPlayer2 =
   not . isPlayer1
 
+-- | Player 1.
 player1 ::
   Player
 player1 =
   Player1
 
+-- | Player 2.
 player2 ::
   Player
 player2 =
   Player2
 
+-- | Folds a player.
 player ::
-  x
-  -> x
-  -> Player
+  x -- ^ If player 1.
+  -> x -- ^ If player 2.
+  -> Player -- ^ The player to fold.
   -> x
 player x _ Player1 =
   x
 player _ x Player2 =
   x
 
+-- | Switches a player from player 1 to player 2 or vice versa.
 alternate ::
-  Player
+  Player -- ^ The player to alternate.
   -> Player
 alternate Player1 =
   Player2
 alternate Player2 =
   Player1
 
+-- | Returns a character symbol denoting each player.
 toSymbol ::
   Player
   -> Char
