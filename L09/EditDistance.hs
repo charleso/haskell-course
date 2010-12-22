88cde63bed1946e0209028231cafe557f4034a15
Tony Morris <tmorris@tmorris.net>
2010-12-22 18:54:23 +1000

    BKTree

--- L09/EditDistance.hs
+++ L09/EditDistance.hs
@@ -1,15 +1,74 @@
 module L09.EditDistance where
 
 import Data.Array
+import qualified Data.Map as M
+import Data.Map (Map)
+import Prelude hiding (any, minimum)
+import Data.Foldable
 
-(<-->) ::
-  Eq a =>
-  [a]
-  -> [a]
+class MetricSpace a where
+  (<-->) ::
+    a
+    -> a
+    -> Int
+
+instance Eq a => MetricSpace [a] where
+  x <--> y =
+    let (t, i, j) = table x y
+    in t ! (i, j)
+
+data BKTree a =
+  Node a (Map Int (BKTree a))
+  | Leaf
+
+null ::
+  BKTree a
+  -> Bool
+null Leaf =
+  True
+null (Node _ _) =
+  False
+
+size ::
+  BKTree a
   -> Int
-x <--> y =
-  let (t, i, j) = table x y
-  in t ! (i, j)
+size Leaf =
+  0
+size (Node _ m) =
+  M.size m
+
+(.:.) ::
+  MetricSpace a =>
+  a
+  -> BKTree a
+  -> BKTree a
+a .:. Leaf =
+  Node a M.empty
+a .:. Node z m =
+  let d = z <--> a
+  in Node z (M.alter (\zz -> Just $! case zz of
+                                       Just w -> a .:. w
+                                       Nothing -> Node a M.empty) d m) -- (pp (a .:.) d m)
+
+asList ::
+  BKTree a
+  -> [a]
+asList Leaf =
+  []
+asList (Node _ m) =
+  fmap snd (M.toList m) >>= asList
+
+(-?-) ::
+  MetricSpace a =>
+  a
+  -> BKTree a
+  -> Bool
+_ -?- Leaf =
+  False
+a -?- Node z m =
+  let d = z <--> a
+  in d == 0 || any ((-?-) a) (d `M.lookup` m)
+
 
 -- do not export
 
