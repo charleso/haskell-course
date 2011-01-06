e7437a341b834a8ffb83f92eaa5503ca58dafed8
Tony Morris <tmorris@tmorris.net>
2011-01-06 17:25:39 +1000

    Fixed bug

--- L09/EditDistance.hs
+++ L09/EditDistance.hs
@@ -1,7 +1,11 @@
-module L09.EditDistance {-
+module L09.EditDistance
 (
   editDistance
-) -} where
+, Edit(..)
+, Edits
+, diff
+, applyDiff
+) where
 
 import Data.Array
 import Prelude hiding (any, minimum)
@@ -24,39 +28,44 @@ data Edit a =
   | Copy
   deriving (Eq, Show)
 
+type Edits a =
+  [Edit a]
+
 diff ::
   Eq a =>
   [a]
   -> [a]
-  -> [Edit a]
+  -> Edits a
 diff a b =
-  let diff _ _ 0 0 = []
-      diff c d p q = let n = t ! (p, q)
-                         n' = n - 1
-                         o = t ! (p - 1, q - 1)
-                         (g, h, i, j, k) = if n' == o
-                                             then
-                                               (Subst (head d), drop 1, drop 1, 1, 1)
-                                             else
-                                               if n' == t ! (p - 1, q)
-                                                 then
-                                                   (Delete, drop 1, id, 1, 0)
-                                                 else
-                                                   if n' == t ! (p, q - 1)
-                                                     then
-                                                       (Insert (head d), id, drop 1, 0, 1)
-                                                     else
-                                                       (Copy, drop 1, drop 1, 1, 1)
-                     in g : diff (h c) (i d) (p - j) (q - k)
+  let di _ _ 0 0 = []
+      di _ d 0 _ = map Insert d
+      di _ _ n 0 = replicate n Delete
+      di c d p q = let n = t ! (p, q)
+                       n' = n - 1
+                       o = t ! (p - 1, q - 1)
+                       (g, h, i, j, k) = if n' == o
+                                           then
+                                             (Subst (head d), drop 1, drop 1, 1, 1)
+                                           else
+                                             if n' == t ! (p - 1, q)
+                                               then
+                                                 (Delete, drop 1, id, 1, 0)
+                                               else
+                                                 if n' == t ! (p, q - 1)
+                                                   then
+                                                     (Insert (head d), id, drop 1, 0, 1)
+                                                   else
+                                                     (Copy, drop 1, drop 1, 1, 1)
+                   in g : di (h c) (i d) (p - j) (q - k)
       t = table a b
       a' = reverse a
       b' = reverse b
       ((x0, y0), (x1, y1)) = bounds t
-  in reverse $ diff a' b' (x1 - x0) (y1 - y0)
+  in reverse $ di a' b' (x1 - x0) (y1 - y0)
 
 applyDiff ::
   [a]
-  -> [Edit a]
+  -> Edits a
   -> [a]
 applyDiff _ [] =
   []
@@ -70,6 +79,8 @@ applyDiff (_:xs) (Delete:es) =
   applyDiff xs es
 applyDiff xs     (Insert c:es) =
   c:applyDiff xs es
+
+
 
 -- do not export
 
@@ -92,5 +103,5 @@ table xs ys  =
       distance (i,0) = i
       distance (i,j) =
         let track = [(1,0,1),(0,1,1),(1,1, if x ! i == y ! j then 0 else 1)]
-        in minimum . fmap (\(p, q, n) -> t ! (i-p,j-q) + n) $ track
+        in minimum . fmap (\(p, q, o) -> t ! (i-p,j-q) + o) $ track
   in t
