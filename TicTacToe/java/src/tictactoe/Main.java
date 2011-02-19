d90cd927233c23ccb34843b4151cba1c7a6d1448
Tony Morris <tmorris@tmorris.net>
2011-02-19 20:07:07 +1000

    game loop

--- TicTacToe/java/src/tictactoe/Main.java
+++ TicTacToe/java/src/tictactoe/Main.java
@@ -1,9 +1,13 @@
 package tictactoe;
 
 import fj.*;
+import fj.data.List;
 import fj.data.Option;
 
+import java.io.Console;
+
 import static fj.Unit.unit;
+import static fj.data.Option.some;
 import static java.lang.System.out;
 
 public final class Main {
@@ -31,6 +35,47 @@ public final class Main {
 
   private static <B> void printBoardSpaces(final F<B, BoardLike> inheritance, final B b) {
     printBoard(inheritance, b, Function.<Position, Character>constant(' '));
+  }
+
+  private static Option<Character> readChar() {
+    final String line = System.console().readLine();
+    return line.isEmpty() ? Option.<Character>none() : some(line.charAt(0));
+  }
+
+  private static <B> void gameLoop(final F<B, BoardLike> inheritance, final F2<Position, B, Unit> move, final B b) {
+    final Player p = inheritance.f(b).whoseTurn();
+    out.println(p + " to move [" + p.toSymbol() + "]");
+    out.println("  [1-9] to Move");
+    out.println("  q to Quit");
+    out.println("  v to view board positions");
+    out.println("  > ");
+
+    readChar().option(new P1<Unit>() {
+      public Unit _1() {
+        out.println("Please make a selection.");
+        gameLoop(inheritance, move, b);
+        return unit();
+      }
+    }, new F<Character, Unit>() {
+      public Unit f(final Character c) {
+        return Position.fromChar(c).option(new P1<Unit>() {
+          public Unit _1() {
+            if(c == 'q' || c == 'Q')
+              out.println("Bye!");
+            else {
+              out.println("Invalid selection. Please try again.");
+              gameLoop(inheritance, move, b);
+            }
+            return unit();
+          }
+        }, new F<Position, Unit>() {
+          public Unit f(final Position d) {
+            return move.f(d, b);
+          }
+        });
+      }
+    });
+
   }
 
   public static void main(final String... args) {
