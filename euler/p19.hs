34cf4003e4b8df55d0c81a2318e41df4b195ff3d
Charles O'Farrell <cofarrell@atlassian.com>
2012-07-21 10:49:29 +1000

    Euler p19

---
+++ euler/p19.hs
@@ -0,0 +1,27 @@
+{-
+You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+-}
+
+import Data.Time.Calendar
+
+
+p19 = filter ((\(_, _, d) -> d == 1) . toGregorian . fst) .
+      dropWhile ((>) (fromGregorian 1901 1 1) . fst) .     
+      takeWhile ((>=) (fromGregorian 2000 12 31) . fst) .
+      filter ((==) Sun . snd) $
+      allDays 
+
+allDays = zip (enumFrom (fromGregorian 1900 1 1)) (cycle (enumFrom Mon))
+
+data DayOfWeek = Mon | Tues | Wed | Thur | Fri | Sat | Sun deriving (Enum, Eq, Show)
+
